dd5b81a0e57879b06a82545c1efa8b6a451cb45a
Jayesh Krishna <jayeshkr152005@gmail.com>
2025-12-27 18:11:11 +0530

    Added analytics

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -12,5 +13,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
